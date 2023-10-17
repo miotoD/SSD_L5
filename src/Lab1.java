5e227be7b4e9fc59cbbb5b1d642f5f5892453999
Aayush <manandharaayush61@gmail.com>
2023-10-17 08:50:28 +0545

    Initial commit

---
+++ src/Lab1.java
@@ -0,0 +1,62 @@
+abstract class Shape{
+
+    protected int sides;
+
+    public int getSides()
+    {
+        return sides;
+    }
+    public void setSides(int sides)
+    {
+        this.sides = sides;
+    }
+
+    /* private int sides
+
+    sides - getter,setter
+
+    getArea()
+    * */
+    abstract public int getArea();
+
+}
+
+class Rectangle extends Shape{
+
+    int breadth;
+    int length;
+
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public void setBreadth(int breadth)
+    {
+        this.breadth = breadth;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    @Override
+    public int getArea() {
+        return length*breadth;
+    }
+}
+
+
+public class Lab1 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setBreadth(3);
+        rectangle.setLength(9);
+        System.out.println(rectangle.getArea());
+    }
+}
---
+++ src/Lab2.java
@@ -0,0 +1,60 @@
+interface Counter
+{
+
+    int countWords(String sentence);
+    int countLetters(String sentence);
+    int countSentence(String sentence);
+}
+class wordProcessor implements Counter
+{
+    @Override
+    public int countWords(String sentence) {
+        if(sentence.equals(null))
+        {
+            return 0;
+        }
+        else {
+            String[] words = sentence.split(" ");
+            int wordsCount = words.length;
+            System.out.println("The number of words is:"+wordsCount);
+            return wordsCount;
+
+        }
+
+
+    }
+
+    @Override
+    public int countLetters(String sentence) {
+        int letterCount = sentence.length();
+        System.out.println("Total numbers of letters:"+letterCount);
+        return 0;
+    }
+
+    @Override
+    public int countSentence(String sentence) {
+        int sentenceCount = 0;
+        String [] sentences = sentence.split("\\.");
+        sentenceCount = sentences.length;
+        System.out.println("The number of sentence is:"+sentenceCount);
+        return sentenceCount;
+    }
+}
+
+
+ class Driver {
+
+     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         String getInput = input.nextLine();
+//         int length = getInput.length();
+//
+//         System.out.println("He wrote:"+getInput);
+//         System.out.println("The length is:"+length);
+         Counter counter = new wordProcessor();
+         counter.countLetters("Nices");
+         counter.countWords("k xa yar");
+         counter.countSentence("Thik xa.Yar.");
+     }
+
+}
